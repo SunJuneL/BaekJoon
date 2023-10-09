686384be93c9362fd4adc5d9aca99bed495fcab5
시드 <115583150+SunJuneL@users.noreply.github.com>
2023-10-09 17:21:40 +0900

    백준 10814
    병합 정렬 + 클래스 이용

--- CPP_BaekJoon/CPP_BaekJoon/CPP_BaekJoon.cpp
+++ CPP_BaekJoon/CPP_BaekJoon/CPP_BaekJoon.cpp
@@ -1,41 +1,122 @@
 ﻿#include <iostream>
 #include <algorithm>
-#include <vector>
 using namespace std;
 
-vector<string> v;
+#define MAX 100000
 
-bool in(vector<string>& v, string str)
+class Couple
 {
-	int count = v.size();
+public:
+	int _age = 0;
+	string _name = "";
 
-	cout << v.size() << endl;
-
-	for (int i = 0; i < v.size(); i++)
+	Couple() {};
+	Couple(int age, string name)
 	{
-		cout << v[i] << endl;
-		if (str.compare(v[i]) != 0)
-			count--;
+		_age = age;
+		_name = name;
 	}
 
-	if (count == 0)
-		return false;
-	else
-		return true;
+	bool operator<= (const Couple &other)
+	{
+		if ((_age) <= other._age)
+			return true;
+		else
+			return false;
+	}
+
+	Couple& operator= (const Couple& other)
+	{
+		_age = other._age;
+		_name = other._name;
+		return *this;
+	}
+};
+
+Couple Arr[MAX];
+Couple Temp[MAX];
+int N;
+
+void Swap(Couple* c, int i, int j)
+{
+	Couple temp = c[i];
+	c[i] = c[j];
+	c[j] = temp;
+}
+
+void Print()
+{
+	for (int i = 0; i < N; i++)
+	{
+        cout << Arr[i]._age << " " << Arr[i]._name << "\n";
+	}
+}
+
+void Merge(int left, int mid, int right)
+{
+    int l = left, r = mid;
+
+    int i = 0;
+
+    while (l < mid && r < right)
+    {
+        if (Arr[l] <= Arr[r])
+        {
+            Temp[i] = Arr[l];
+            l++;
+        }
+        else
+        {
+            Temp[i] = Arr[r];
+            r++;
+        }
+        i++;
+    }
+
+    while (l < mid)
+    {
+        Temp[i] = Arr[l];
+        l++;
+        i++;
+    }
+
+    while (r < right)
+    {
+        Temp[i] = Arr[r];
+        r++;
+        i++;
+    }
+
+    for (int j = 0; j < i; j++)
+        Arr[left + j] = Temp[j];
+}
+
+void MergeSort(int left, int right)
+{
+    if (left + 1 >= right)
+        return;
+
+    int mid = (right + left) / 2;
+    MergeSort(left, mid);
+    MergeSort(mid, right);
+    Merge(left, mid, right);
 }
 
 int main()
 {
-	int N;
-	cin >> N;
+    // cout << "N 입력 : ";
+    cin >> N;
 
-	for (int i = 0; i < N; i++)
-	{
-		string str;
-		cin >> str;
+    // cout << "\n-------------------------- 배열 입력 --------------------------" << endl;
 
-		v.push_back(str);
-	}
+    for (int i = 0; i < N; i++)
+    {
+        cin >> Arr[i]._age >> Arr[i]._name;
+    }
 
-	cout << in(v, "no");
+    MergeSort(0, N);
+
+    // cout << "\n--------------------------------결과------------------------------" << endl;
+
+    Print();
 }
