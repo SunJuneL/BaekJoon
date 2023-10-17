f2fecf439499d26db9d5f2b1e1b673f13fda2b17
시드 <115583150+SunJuneL@users.noreply.github.com>
2023-10-17 13:34:30 +0900

    23.10.17

--- CPP_BaekJoon/CPP_BaekJoon/CPP_BaekJoon.cpp
+++ CPP_BaekJoon/CPP_BaekJoon/CPP_BaekJoon.cpp
@@ -1,122 +1,48 @@
 ﻿#include <iostream>
 #include <algorithm>
+#include <set>
 using namespace std;
 
-#define MAX 100000
-
-class Couple
+enum Range
 {
-public:
-	int _age = 0;
-	string _name = "";
-
-	Couple() {};
-	Couple(int age, string name)
-	{
-		_age = age;
-		_name = name;
-	}
-
-	bool operator<= (const Couple &other)
-	{
-		if ((_age) <= other._age)
-			return true;
-		else
-			return false;
-	}
-
-	Couple& operator= (const Couple& other)
-	{
-		_age = other._age;
-		_name = other._name;
-		return *this;
-	}
+	MAX = 1000000,
 };
 
-Couple Arr[MAX];
-Couple Temp[MAX];
-int N;
-
-void Swap(Couple* c, int i, int j)
-{
-	Couple temp = c[i];
-	c[i] = c[j];
-	c[j] = temp;
-}
-
-void Print()
-{
-	for (int i = 0; i < N; i++)
-	{
-        cout << Arr[i]._age << " " << Arr[i]._name << "\n";
-	}
-}
-
-void Merge(int left, int mid, int right)
-{
-    int l = left, r = mid;
-
-    int i = 0;
-
-    while (l < mid && r < right)
-    {
-        if (Arr[l] <= Arr[r])
-        {
-            Temp[i] = Arr[l];
-            l++;
-        }
-        else
-        {
-            Temp[i] = Arr[r];
-            r++;
-        }
-        i++;
-    }
-
-    while (l < mid)
-    {
-        Temp[i] = Arr[l];
-        l++;
-        i++;
-    }
-
-    while (r < right)
-    {
-        Temp[i] = Arr[r];
-        r++;
-        i++;
-    }
-
-    for (int j = 0; j < i; j++)
-        Arr[left + j] = Temp[j];
-}
-
-void MergeSort(int left, int right)
-{
-    if (left + 1 >= right)
-        return;
-
-    int mid = (right + left) / 2;
-    MergeSort(left, mid);
-    MergeSort(mid, right);
-    Merge(left, mid, right);
-}
+set<int> s;
+int arr[MAX];
+int ans[MAX];
 
 int main()
 {
-    // cout << "N 입력 : ";
-    cin >> N;
+	int N;
+	cin >> N;
 
-    // cout << "\n-------------------------- 배열 입력 --------------------------" << endl;
+	for (int i = 0; i < N; i++)
+	{
+		int num;
+		cin >> num;
 
-    for (int i = 0; i < N; i++)
-    {
-        cin >> Arr[i]._age >> Arr[i]._name;
-    }
+		s.insert(num);
+	}
 
-    MergeSort(0, N);
+	int index = 0;
+	for (set<int>::iterator iter = s.begin(); iter != s.end(); iter++)
+	{
+		arr[index] = *iter;
+		index++;
+	}
 
-    // cout << "\n--------------------------------결과------------------------------" << endl;
+	int M;
+	cin >> M;
 
-    Print();
+	for (int i = 0; i < M; i++)
+	{
+		int num;
+		cin >> num;
+		
+		if (arr[lower_bound(arr, arr + N, num) - arr] == num)
+			cout << "1 ";
+		else
+			cout << "0 ";
+	}
 }
